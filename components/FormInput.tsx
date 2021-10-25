1f26da9d71b2f59e3fae5b5ef506e2dd6932ee77
Eugene Bondarenko <eugene.bondarenko@itomy.ch>
2021-10-25 18:40:24 +0300

    Resolve perfomance issue for input component

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 type Props = {
   id: string,
   label: string,
@@ -7,25 +9,34 @@ type Props = {
 
 function wait(milliseconds: number) {
   const start = new Date().getTime();
+
   while (new Date().getTime() - start < milliseconds) {}
 }
-export default function FormInput({id, label, errorMessage, onInput}: Props) {
+
+export default memo(function FormInput({id, label, errorMessage, onInput}: Props) {
   // This component is a bit slow to render...
   wait(25);
 
   return (
     <div className="flex items-center">
       <label htmlFor={id} className="flex-1">{label}</label>
+
       <div className="flex-1">
         <input
           type="text"
           id={id}
           name={id}
           className="w-full border rounded p-2"
-          onInput={(e) => onInput(e.currentTarget.value)}
+          onInput={({ currentTarget }) => {
+            onInput(currentTarget.value);
+
+            // For even better performance:
+            // setTimeout(() => onInput(currentTarget.value));
+          }}
         />
+
         {errorMessage && <p className="text-sm text-red-600">{errorMessage}</p>}
       </div>
     </div>
   );
-}
+})
